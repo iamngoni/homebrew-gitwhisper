73ff529e9f4295c212969253fd281ae0e1b024d7
Ngonidzashe Mangudya <imngonii@gmail.com>
2025-06-25 16:13:13 +0200

    feat: ✨ Add Homebrew formula for gitwhisper CLI tool

---
+++ Formula/gitwhisper.rb
@@ -0,0 +1,11 @@
+class Gitwhisper < Formula
+  desc "AI-assisted git commit CLI"
+  homepage "https://github.com/ngoni/gitwhisper"
+  version "0.1.0"
+  url "https://github.com/ngoni/gitwhisper/releases/download/v0.1.0/gitwhisper-macos-latest.tar.gz"
+  sha256 "<TO-BE-UPDATED>"
+
+  def install
+    bin.install "gitwhisper"
+  end
+end
